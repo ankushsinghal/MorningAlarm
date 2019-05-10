0bb3e6fddf468c0f26d3f7ded5c5279daa9c9e4f
Ankush Singhal <ankushsinghal1995@gmail.com>
2019-05-11 03:23:12 +0530

    This the the basic application
    1) Made the changes to add a TimePicker and a Button to the layout.
    2) When the button is pressed, 4 alarms are now set at 10 minute
    intervals before the specified time on the time picker.

--- app/src/main/java/io/github/ankushsinghal/morningalarm/MainActivity.java
+++ app/src/main/java/io/github/ankushsinghal/morningalarm/MainActivity.java
@@ -1,7 +1,12 @@
 package io.github.ankushsinghal.morningalarm;
 
+import android.content.Intent;
+import android.os.Build;
+import android.provider.AlarmClock;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TimePicker;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,55 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TimePicker timePicker = findViewById(R.id.simpleTimePicker);
+        if(Build.VERSION.SDK_INT >= 23){
+            timePicker.setHour(8);
+            timePicker.setMinute(0);
+        }
+        else{
+            timePicker.setCurrentHour(8);
+            timePicker.setCurrentMinute(0);
+        }
+    }
+
+    public void onClick(View view){
+
+        TimePicker timePicker = findViewById(R.id.simpleTimePicker);
+        int hour, minute, intentHour, intentMinute;
+
+        if(Build.VERSION.SDK_INT >= 23){
+            hour = timePicker.getHour();
+            minute = timePicker.getMinute();
+        }
+        else{
+            hour = timePicker.getCurrentHour();
+            minute = timePicker.getCurrentMinute();
+        }
+
+        intentHour = hour;
+        intentMinute = minute;
+
+        Intent intent = new Intent(AlarmClock.ACTION_SET_ALARM);
+        intent.putExtra(AlarmClock.EXTRA_SKIP_UI, true);
+        intent.putExtra(AlarmClock.EXTRA_VIBRATE, true);
+
+        /*
+         *  We want to make 4 alarms at 10 minute intervals before the
+         *  specified time in the time picker, so that I get up on time
+         */
+
+        for(int i=0; i < 4; i++){
+
+            intent.putExtra(AlarmClock.EXTRA_HOUR, intentHour);
+            intent.putExtra(AlarmClock.EXTRA_MINUTES, intentMinute);
+            startActivity(intent);
+
+            intentMinute -= 10;
+            if(intentMinute < 0){
+                intentMinute += 60;
+                intentHour--;
+            }
+        }
     }
 }
