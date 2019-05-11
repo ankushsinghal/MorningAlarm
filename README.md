53152eab1a3697759118abd61a8ac344cc5512b5
Ankush Singhal <ankushsinghal1995@gmail.com>
2019-05-11 21:18:46 +0530

    Added Number Picker to choose the number of alarms to be set
    1) Added Number Picker to choose the number of alarms to be set
    2) Minimum number of alarms that can be set is 1 and maximum number is
    10
    3) Default number of alarms is 4

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # MorningAlarm
 An android application to create a series of alarms with the click of a button, so that I can wake up in the morning.
 
-* The layout contains a Time Picker and a button to set the alarm.
-* When we press the button, 4 alarms are set at 10 minute intervals before the specified time on the Time Picker.
+* The layout contains a Time Picker, a Number Picker to select the number of alarms to be set, and a button to set the alarm.
+* The maximum number of alarms that can be set are 10
+* When we press the button, the selected number of alarms are set at 10 minute intervals before the specified time on the Time Picker.
--- app/src/main/java/io/github/ankushsinghal/morningalarm/MainActivity.java
+++ app/src/main/java/io/github/ankushsinghal/morningalarm/MainActivity.java
@@ -6,9 +6,17 @@ import android.provider.AlarmClock;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.NumberPicker;
 import android.widget.TimePicker;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final int ALARM_INTERVAL = 10;
+    public static final int MIN_NUM_ALARMS = 1;
+    public static final int MAX_NUM_ALARMS = 10;
+    public static final int DEFAULT_NUM_ALARMS = 4;
+    public static final int DEFAULT_HOUR = 8;
+    public static final int DEFAULT_MINUTE = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,13 +25,18 @@ public class MainActivity extends AppCompatActivity {
 
         TimePicker timePicker = findViewById(R.id.simpleTimePicker);
         if(Build.VERSION.SDK_INT >= 23){
-            timePicker.setHour(8);
-            timePicker.setMinute(0);
+            timePicker.setHour(DEFAULT_HOUR);
+            timePicker.setMinute(DEFAULT_MINUTE);
         }
         else{
-            timePicker.setCurrentHour(8);
-            timePicker.setCurrentMinute(0);
+            timePicker.setCurrentHour(DEFAULT_HOUR);
+            timePicker.setCurrentMinute(DEFAULT_MINUTE);
         }
+
+        NumberPicker numberPicker = findViewById(R.id.numberPicker);
+        numberPicker.setMinValue(MIN_NUM_ALARMS);
+        numberPicker.setMaxValue(MAX_NUM_ALARMS);
+        numberPicker.setValue(DEFAULT_NUM_ALARMS);       // Default number of alarms
     }
 
     public void onClick(View view){
@@ -48,17 +61,20 @@ public class MainActivity extends AppCompatActivity {
         intent.putExtra(AlarmClock.EXTRA_VIBRATE, true);
 
         /*
-         *  We want to make 4 alarms at 10 minute intervals before the
+         *  We want to make a number of alarms at 10 minute intervals before the
          *  specified time in the time picker, so that I get up on time
          */
 
-        for(int i=0; i < 4; i++){
+        NumberPicker numberPicker = findViewById(R.id.numberPicker);
+        int numAlarms = numberPicker.getValue();
+
+        for(int i=0; i < numAlarms; i++){
 
             intent.putExtra(AlarmClock.EXTRA_HOUR, intentHour);
             intent.putExtra(AlarmClock.EXTRA_MINUTES, intentMinute);
             startActivity(intent);
 
-            intentMinute -= 10;
+            intentMinute -= ALARM_INTERVAL;
             if(intentMinute < 0){
                 intentMinute += 60;
                 intentHour--;
